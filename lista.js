5031ace154f66bfbeecd9401fdc0713589263037
Admision <admision@ort>
2022-03-31 12:12:43 -0300

    lista

---
+++ lista.js
@@ -0,0 +1,14 @@
+var lista=[11, 33, 2, -1, 110, 99, 8];
+var numMayor = 0;
+var segundoMayor = 0;
+
+for (let i = 0; i < lista.length; i++) {
+    let e = lista[i];
+    if (e % 2 !=0 && e> numMayor){
+        numMayor= lista[i];
+    }
+    else if (e % 2 !=0 && e <numMayor && e>segundoMayor){
+        segundoMayor = lista[i];
+    }
+    console.log(segundoMayor);
+}
