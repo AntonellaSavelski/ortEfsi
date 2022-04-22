23d730074e6ec070e63f5a21cae9896aef19df6b
Antonella Savelski <antonellasavelski@gmail.com>
2022-04-22 16:52:54 -0300

    avance bingo

--- tp1.js
+++ tp1.js
@@ -19,26 +19,31 @@ app.post("/numero_aleatorio", function (req, res) {
 	res.send([Math.round(Math.random() * (req.body.num- min) + min)])
 });
 app.post("/iniciar_juego", function (req, res) {
-	console.log(req.body)
-    const arrayCartones = [];
-    const carton = [];
-    for (let i = 0; i < req; i++) {
-        let carton = { 
-            for (let i = 0; i < 10; i++) {
-                Math.round(Math.random() * (10- 1) + 1)
-                
-            }
-            numeroDeCarton: i,
-        };
-       
+	console.log(req.body.cartones);
+    let arrayCartones = []
+    for (let i = 0;i<req.body.cartones;i++) {
+        let carton = []
+        for (let j=0; j<10;j++) {
+            let num = Math.round(Math.random() * (99- 1) + 1)
+            for (let a = 0; a < 10; a++) {
+                if (num === carton[a]){
+                    num = Math.round(Math.random() * (99- 1) + 1)
+                }
+            } 
+            carton[j]= num;
+        }
         arrayCartones.push(carton);
-    
     }
-	res.send(arrayCartones)
+	res.send(arrayCartones);
 });
 
-app.get("/mi_endpoint", function (req, res) {
-    res.send("respuesta");
+app.get("/obtener_carton", function (req, res) {
+    let jugadores = [];
+    jugadores = req.body.jugadores
+    for (let i = 0; i < jugadores.length; i++) {
+        console.log(`Carton ${i+1}:${req.body.jugadores[i]}`)        
+    }
+    res.send("Los jugadores ya tienen sus cartones asignados, comenzemos.");
 });
 
 app.listen(PORT, function(err){
